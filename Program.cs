43814874346b73bf74111d673d973eb30f314575
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-02 11:12:13 +0530

    [sanjana] Add : Addtion Of two numbers

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Sanjana");
+﻿using System;
+namespace Demoexample
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+           int a ,b ;
+            Console.WriteLine("Please Enter First number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Second number");
+            b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b ;
+            Console.WriteLine("Addition of two number is " + c);
+            Console.ReadKey();
+        }
+    }
+}
