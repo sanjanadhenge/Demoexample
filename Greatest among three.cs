709fe9232f4fcec93e220f8c9f1dfe4f5905086b
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-02 11:55:40 +0530

    [sanjana] Add : Greatest among three

---
+++ Greatest among three.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demoexample
+{
+    internal class Greatest_among_three
+    {
+        static void Main(string[] args)
+        {
+            int a, b,c;
+            Console.WriteLine("Please Enter First number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Second number");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Third number");
+            c = Convert.ToInt32(Console.ReadLine());
+           if ( a > b && a > c)
+            {
+                Console.WriteLine("The greatest among three is " + a);
+
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine("The greatest among three is " + b);
+            }
+            else
+            {
+                Console.WriteLine("The greatest among three is " + c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
